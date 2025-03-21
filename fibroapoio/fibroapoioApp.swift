c725cfe8b7f26cc3ff685883bff5d3e9b7382184
Italo Teófilo Filho <italo.filho@imts.com.br>
2025-03-21 11:06:46 +0000

    Initial Commit

---
+++ fibroapoio/fibroapoioApp.swift
@@ -0,0 +1,17 @@
+//
+//  fibroapoioApp.swift
+//  fibroapoio
+//
+//  Created by Italo Teofilo Filho on 21/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct fibroapoioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
